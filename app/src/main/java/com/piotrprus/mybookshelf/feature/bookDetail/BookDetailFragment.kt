07caeb7b48110ee8a5defdc314857c471119a5fa
PiotrPrus <prus.piotr@gmail.com>
2019-12-17 22:11:16 +0100

    Prepare functions in detailVM, edit styles and update dependency for material components

--- app/src/main/java/com/piotrprus/mybookshelf/feature/bookDetail/BookDetailFragment.kt
+++ app/src/main/java/com/piotrprus/mybookshelf/feature/bookDetail/BookDetailFragment.kt
@@ -1,15 +1,40 @@
 package com.piotrprus.mybookshelf.feature.bookDetail
 
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import com.piotrprus.mybookshelf.R
 import com.piotrprus.mybookshelf.base.BaseVMFragment
 import com.piotrprus.mybookshelf.base.LayoutResId
 import com.piotrprus.mybookshelf.databinding.FragmentBookDetailBinding
+import com.piotrprus.mybookshelf.feature.main.MainSharedViewModel
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 @LayoutResId(R.layout.fragment_book_detail)
 class BookDetailFragment
     : BaseVMFragment<BookDetailViewModel, FragmentBookDetailBinding>(BookDetailViewModel::class) {
 
+    private val mainSharedViewModel: MainSharedViewModel by sharedViewModel()
+
     override fun start() {
         binding.viewModel = viewModel
+        viewModel.apply {
+            navigateToBookListEvent.observeEvent {
+                findNavController().navigate(R.id.bookListFragment)
+            }
+            showErrorSnackbarEvent.observeEvent { msg -> showSnackbar(msg) }
+            bookMediatorLD.observe { }
+        }
+        mainSharedViewModel.mainFabClickEvent.observeEvent {
+            viewModel.onConfirmationClicked()
+        }
+    }
+
+    private fun showSnackbar(msg: String) {
+        Snackbar.make(binding.root, msg, Snackbar.LENGTH_SHORT)
+            .setAnchorView(requireActivity().findViewById<FloatingActionButton>(R.id.mainFAB))
+            .show()
     }
 }
--- app/src/main/java/com/piotrprus/mybookshelf/feature/bookDetail/BookDetailViewModel.kt
+++ app/src/main/java/com/piotrprus/mybookshelf/feature/bookDetail/BookDetailViewModel.kt
@@ -1,6 +1,70 @@
 package com.piotrprus.mybookshelf.feature.bookDetail
 
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.piotrprus.mybookshelf.common.data.model.Book
+import com.piotrprus.mybookshelf.common.data.repository.BookRepository
+import com.piotrprus.mybookshelf.common.utils.event.DataEventEmitter
+import com.piotrprus.mybookshelf.common.utils.event.EventEmitter
+import com.piotrprus.mybookshelf.common.utils.event.emit
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
-class BookDetailViewModel: ViewModel() {
+class BookDetailViewModel(private val bookRepository: BookRepository) : ViewModel() {
+
+    val bookTitle = MutableLiveData<String>()
+    val bookAuthor = MutableLiveData<String>()
+    val bookISBN = MutableLiveData<String>()
+    val bookPages = MutableLiveData<String>()
+    val bookRating = MutableLiveData<Float>()
+    val bookMediatorLD = MediatorLiveData<Book>()
+    val showErrorSnackbarEvent = DataEventEmitter<String>()
+    val navigateToBookListEvent = EventEmitter()
+
+    init {
+        setupBookMediatorLD()
+    }
+
+    private fun setupBookMediatorLD() {
+        bookMediatorLD.value = Book()
+        bookMediatorLD.addSource(bookTitle) {
+            bookMediatorLD.value = bookMediatorLD.value?.copy(title = it)
+        }
+        bookMediatorLD.addSource(bookAuthor) {
+            bookMediatorLD.value = bookMediatorLD.value?.copy(author = it)
+        }
+        bookMediatorLD.addSource(bookISBN) {
+            bookMediatorLD.value = bookMediatorLD.value?.copy(isbnNumber = it)
+        }
+        bookMediatorLD.addSource(bookPages) {
+            bookMediatorLD.value = bookMediatorLD.value?.copy(pages = it.toInt())
+        }
+        bookMediatorLD.addSource(bookRating) {
+            bookMediatorLD.value = bookMediatorLD.value?.copy(rating = it.roundToInt())
+        }
+
+    }
+
+    fun onConfirmationClicked() {
+        bookMediatorLD.value?.let { book ->
+            viewModelScope.launch {
+                try {
+                    bookRepository.addBook(book)
+                    navigateToBookListEvent.emit()
+                } catch (e: Exception) {
+                    showErrorSnackbarEvent.emit(e.localizedMessage ?: "Failed to add book")
+                }
+            }
+        }
+    }
+
+    fun setBook(book: Book) {
+        bookTitle.value = book.title
+        bookAuthor.value = book.author
+        bookISBN.value = book.isbnNumber
+        bookPages.value = book.pages.toString()
+        bookRating.value = book.rating.toFloat()
+    }
 }
